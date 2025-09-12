0b39519947b52ec18bd4a537ae1ccc0accf41b81
SaraFerrucho <saraferrucho@uptc.edu.co>
2025-09-12 11:15:01 -0500

    Se creo la clase Module y se construyo junto a Course

--- src/main/java/co/edu/uptcsoft/model/Course.java
+++ src/main/java/co/edu/uptcsoft/model/Course.java
@@ -1,5 +1,54 @@
 package co.edu.uptcsoft.model;
 
-public class Course {
+import java.util.List;
 
+public class Course {
+    private String id;
+    private String title;
+    private String description;
+    private List<Module> modules;
+
+    @Override
+    public String toString() {
+        return "Course [id=" + id + ", title=" + title + ", description=" + description + ", modules=" + modules + "]";
+    }
+
+    public Course(String id, String title, String description, List<Module> modules) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.modules = modules;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Module> getModules() {
+        return modules;
+    }
+
+    public void setModules(List<Module> modules) {
+        this.modules = modules;
+    }
 }
---
+++ src/main/java/co/edu/uptcsoft/model/Module.java
@@ -0,0 +1,45 @@
+package co.edu.uptcsoft.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Module {
+    private String id;
+    private String title;
+    private List<Lesson> lessons;
+
+    @Override
+    public String toString() {
+        return "Module [id=" + id + ", title=" + title + ", lessons=" + lessons + "]";
+    }
+
+    public Module(String id, String title, List<Lesson> lessons) {
+        this.id = id;
+        this.title = title;
+        lessons = new ArrayList<>();;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Lesson> getLessons() {
+        return lessons;
+    }
+
+    public void setLessons(List<Lesson> lessons) {
+        this.lessons = lessons;
+    }
+}
